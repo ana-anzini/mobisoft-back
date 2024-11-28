5937c6136bd82037229b4d1d7e779b1c7fb1be20
ana.anzini <anabeanzini@gmail.com>
2024-11-28 09:29:56 -0300

    FinancialServiceTest

--- src/test/java/com/mobisoft/mobisoftapi/service/FinancialServiceTest.java
+++ src/test/java/com/mobisoft/mobisoftapi/service/FinancialServiceTest.java
@@ -60,16 +60,17 @@ class FinancialServiceTest {
     void setUp() {
         MockitoAnnotations.openMocks(this);
 
-        // Mock do retorno de métodos de serviço
-        when(administrationService.findByUserGroup()).thenReturn(administration);
-        when(deliveriesService.findByProjectId(anyLong())).thenReturn(deliveries);
+        when(administration.getTax()).thenReturn(new BigDecimal("10.0"));
+        when(administration.getAdditionalFinancial()).thenReturn(new BigDecimal("5.0"));
+        when(administration.getAdditionalProjectDesigner()).thenReturn(new BigDecimal("3.0"));
+        when(administration.getAdditionalSeller()).thenReturn(new BigDecimal("2.0"));
+        when(administration.getAdditionalAssembler()).thenReturn(new BigDecimal("1.0"));
     }
 
     @Test
     void testCreateFinancial() {
         when(financialRepository.save(any(Financial.class))).thenReturn(financial);
 
-        // Configuração do FinancialDTO
         when(financialDTO.getInstallmentsNumber()).thenReturn(5);
         when(financialDTO.getFirstPayment()).thenReturn(Calendar.getInstance());
         when(financialDTO.getPaymentType()).thenReturn(PaymentType.DEBIT);
@@ -81,10 +82,8 @@ class FinancialServiceTest {
         when(financialDTO.getTotalSeller()).thenReturn(new BigDecimal("150.00"));
         when(financialDTO.getTotalProfit()).thenReturn(new BigDecimal("1000.00"));
 
-        // Executando o método de criação
         Financial result = financialService.createFinancial(financialDTO);
 
-        // Verificando o comportamento esperado
         assertNotNull(result);
         verify(financialRepository, times(1)).save(any(Financial.class));
     }
@@ -94,10 +93,8 @@ class FinancialServiceTest {
         Long financialId = 1L;
         when(financialRepository.findById(financialId)).thenReturn(Optional.of(financial));
 
-        // Executando a busca do Financial
         Financial result = financialService.findById(financialId);
 
-        // Verificando o comportamento esperado
         assertNotNull(result);
         verify(financialRepository, times(1)).findById(financialId);
     }
@@ -107,7 +104,6 @@ class FinancialServiceTest {
         Long financialId = 1L;
         when(financialRepository.findById(financialId)).thenReturn(Optional.empty());
 
-        // Verificando a exceção
         assertThrows(FinancialNotFoundException.class, () -> financialService.findById(financialId));
     }
 
@@ -116,10 +112,8 @@ class FinancialServiceTest {
         Long projectId = 1L;
         when(financialRepository.findByProjectId(projectId)).thenReturn(financial);
 
-        // Executando a busca do Financial por Project ID
         Financial result = financialService.findByProjectId(projectId);
 
-        // Verificando o comportamento esperado
         assertNotNull(result);
         verify(financialRepository, times(1)).findByProjectId(projectId);
     }
@@ -130,17 +124,14 @@ class FinancialServiceTest {
         when(financialRepository.findByProjectId(projectId)).thenReturn(financial);
         when(financialRepository.save(any(Financial.class))).thenReturn(financial);
 
-        // Configuração do FinancialDTO para atualização
         when(financialDTO.getInstallmentsNumber()).thenReturn(10);
         when(financialDTO.getFirstPayment()).thenReturn(Calendar.getInstance());
         when(financialDTO.getPaymentType()).thenReturn(PaymentType.DEBIT);
         when(financialDTO.getDiscount()).thenReturn(new BigDecimal("200.00"));
         when(financialDTO.getAdditionalExpenses()).thenReturn(new BigDecimal("100.00"));
 
-        // Executando a atualização do Financial
         Financial result = financialService.updateFinancial(projectId, financialDTO);
 
-        // Verificando o comportamento esperado
         assertNotNull(result);
         verify(financialRepository, times(1)).save(any(Financial.class));
     }
@@ -150,11 +141,42 @@ class FinancialServiceTest {
         Long financialId = 1L;
         when(financialRepository.findById(financialId)).thenReturn(Optional.of(financial));
 
-        // Executando a exclusão do Financial
         financialService.deleteFinancial(financialId);
 
-        // Verificando o comportamento esperado
         verify(financialRepository, times(1)).delete(any(Financial.class));
+    }
+    
+    @Test
+    void testCalculateTotalProject() {
+        Long projectId = 1L;
+        
+        when(financialRepository.findByProjectId(projectId)).thenReturn(financial);
+        when(administrationService.findByUserGroup()).thenReturn(administration);
+        when(deliveriesService.findByProjectId(projectId)).thenReturn(deliveries);
+        
+        when(administration.getTax()).thenReturn(new BigDecimal("10.0"));
+        when(administration.getAdditionalFinancial()).thenReturn(new BigDecimal("5.0"));
+        when(administration.getAdditionalProjectDesigner()).thenReturn(new BigDecimal("3.0"));
+        when(administration.getAdditionalSeller()).thenReturn(new BigDecimal("2.0"));
+        when(administration.getAdditionalAssembler()).thenReturn(new BigDecimal("1.0"));
+
+        when(deliveries.getFreight()).thenReturn(new BigDecimal("100.0"));
+
+        when(financial.getTotalCusts()).thenReturn(new BigDecimal("3000.0"));
+
+        Financial result = financialService.calculateTotalProject(projectId);
+
+        assertNotNull(result);
+        verify(financialRepository, times(1)).findByProjectId(projectId);
+    }
+    
+    @Test
+    void testSave() {
+        when(financialRepository.save(financial)).thenReturn(financial);
+
+        financialService.save(financial);
+
+        verify(financialRepository, times(1)).save(financial);
     }
 
     @Test
@@ -162,7 +184,6 @@ class FinancialServiceTest {
         Long financialId = 1L;
         when(financialRepository.findById(financialId)).thenReturn(Optional.empty());
 
-        // Verificando a exceção
         assertThrows(FinancialNotFoundException.class, () -> financialService.deleteFinancial(financialId));
     }
 }
