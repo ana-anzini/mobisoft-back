d93033dcfb45e57c48f19aaff1865b07a4ebd10c
ana.anzini <anabeanzini@gmail.com>
2024-11-29 01:00:31 -0300

    Update SecurityConfigurations.java

--- src/main/java/com/mobisoft/mobisoftapi/configs/security/SecurityConfigurations.java
+++ src/main/java/com/mobisoft/mobisoftapi/configs/security/SecurityConfigurations.java
@@ -2,14 +2,11 @@ package com.mobisoft.mobisoftapi.configs.security;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -18,16 +15,12 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.web.cors.CorsConfiguration;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfigurations {
 
-	@Autowired
-    SecurityFilter securityFilter;
-	
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity, SecurityFilter securityFilter) throws Exception {
         return  httpSecurity
-        		.cors(cors -> cors.configurationSource(request -> {
+                .cors(cors -> cors.configurationSource(request -> {
                     CorsConfiguration config = new CorsConfiguration();
                     config.setAllowedOrigins(List.of("http://localhost:3000", "https://mobisoft-frontend.vercel.app/", "https://mobisoft.site/")); 
                     config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE")); 
@@ -41,24 +34,23 @@ public class SecurityConfigurations {
                         .requestMatchers(ENDPOINTS_WITH_AUTHENTICATION_NOT_REQUIRED).permitAll()
                         .requestMatchers("/actuator/**").permitAll()
                         .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html").permitAll()
-                        //.requestMatchers(ENDPOINTS_TO_ADMIN).hasRole("ADMIN")
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
-    
+
     public static final String [] ENDPOINTS_WITH_AUTHENTICATION_NOT_REQUIRED = {
             "/auth/login",
             "/auth/register",
             "auth/isAuthenticated"
     };
-    
+
     public static final String [] ENDPOINTS_TO_ADMIN = {
             "/categories",
             "/categories/**",
     };
-    
+
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
